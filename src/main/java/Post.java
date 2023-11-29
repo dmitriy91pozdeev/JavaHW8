4ed057b41d59d2cbd1b083fb99022b2df99b23d8
Name Surname <dmitriy91pozdeev@mail.ru>
2023-11-30 01:00:40 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surename;
+    public boolean subscription;
+
+    public FormDate birthday;
+}
+
